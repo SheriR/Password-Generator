73f6de48569ae41881c0b56138b42219b679a718
Sheri Rhoades <sheri_rhoades@yahoo.com>
2019-12-16 05:36:23 -0600

    add prompt for character length to js file

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-//setting up the variables
-var upperEl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var lowerEl = 'abcdefghijklmnopqrstuvwxyz';
-var numEl = '0123456789';
-var numEl = '0123456789';
+//setting up variables for different choices for character passwords
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var number = '0123456789';
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', ':', '"', '<', '>', '?', '"'];
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// creating bucket for user's choices
-var userChoice = {};
+//creating an object (bucket) to save user choices
+var userChoices = {};
 
-// prompt for specified character length
-var charLength = parseInt(prompt("Please type in a number between 8 and 128."));
+//prompting the user to spefific character length
+var charLength = parseInt(prompt("How many characters do you want your password?  Please type in a number between 8 and 128."));
 
-
-
+// userChoice["charLength"] = charLength; 
 console.log(charLength)
+
+
 
